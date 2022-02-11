933a605a17c6f669369efb17374ee1ea796c7885
MatthewHoque <hoque.matt@gmail.com>
2022-02-11 11:18:05 -0800

    twoSum finished

--- TwoSum/Solution.py
+++ TwoSum/Solution.py
@@ -7,7 +7,6 @@ class Solution:
         numDB = {}
         indexDB={}
         for i in range(len(nums)):
-            print(i)
             if numDB.get(nums[i]) == None: #numDB.get worked but numDB[nums[i]] did not, wondering why.
                 numDB[target-nums[i]]=nums[i]
                 indexDB[target-nums[i]]=i
@@ -17,6 +16,6 @@ class Solution:
 
 
 sol = Solution()
-nums = [2, 5, 10, 11]
-target = 7
+nums = [-1,-2,2, 5, 10, 11]
+target = 8
 print(Solution.twoSum(sol, nums, target))
