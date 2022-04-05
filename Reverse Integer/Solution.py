7adf37aea4142efd621d90a9f4cf68043f2ebf9f
MatthewHoque <hoque.matt@gmail.com>
2022-04-05 10:51:28 -0700

    accepted, needs cleanup

--- Reverse Integer/Solution.py
+++ Reverse Integer/Solution.py
@@ -4,37 +4,57 @@ class Solution:
         negative = x < 0
         numDigits = 0
         comparator = -1
-        truncateds = {}
-        numOrder = {}
+        truncateds = []
+        numOrder = []
 
         if not negative:
             x *= -1
 
-        print(x)
         if x > -10:
             return x if negative else -x
 
         # print(x)
-        while comparator > x:
+        while comparator >= x:
             comparator *= 10
             numDigits += 1
-        print("numdigts ", numDigits)
 
         for i in range(numDigits):
-            truncateds[i] = (int)(x/10**i)
+            truncateds.append((int)(x/10**i))
 
+        streakOfZeros = True
+        finalNumDigits = numDigits
         for i in range(numDigits-1):
-            numOrder[i] = truncateds[i]-truncateds[i+1]*10
-        numOrder[numDigits-1] = truncateds[numDigits-1]
+            trunc = truncateds[i]-truncateds[i+1]*10
+            print(trunc)
+            if not streakOfZeros:
+                numOrder.append(trunc)
+            elif trunc != 0:
+                streakOfZeros = False
+                numOrder.append(trunc)
+            else:
+                finalNumDigits -= 1
+
+            # if streakOfZeros == i-1 and numOrder[i] == 0:
+            #     streakOfZeros = i
+        for i in range(numDigits-finalNumDigits):
+            truncateds.pop(0)
+        numDigits = finalNumDigits
+        print("nd", numDigits)
+        print("numOrder", numOrder)
+        numOrder.append(truncateds[numDigits-1])
+        print("numOrder", numOrder)
+        # if streakOfZeros == i-1 and numOrder[numDigits-1] == 0:
+        #     streakOfZeros = i
+
+        print(truncateds)
+        print(numOrder)
+
+        # if streakOfZeros != -1:
+        #     numDigits -= streakOfZeros-1
 
         reversedFit = 0
         for i in range(0, numDigits-1):
             reversedFit += numOrder[i]*10**(numDigits-i-2)
-
-        print(reversedFit)
-        print(numDigits)
-        print(truncateds)
-        print(numOrder)
 
         corrector = 1 if negative else -1
         if(reversedFit < MIN_32_BIT_TRUNCATED):
@@ -45,12 +65,8 @@ class Solution:
                 corrector = 0
         return corrector*(reversedFit*10+numOrder[numDigits-1])
 
-        print(reversedFit)
-        # fullyReversed =
-        # print(fullyReversed)
-
 
 sol = Solution()
-# print(sol.reverse(1111111111))
-print(sol.reverse(-9463847412))
+print(sol.reverse(-1100000))
+# print(sol.reverse(-8463847412))
 # print(sol.reverse(123))
--- Reverse Integer/testing.py
+++ Reverse Integer/testing.py
@@ -1,9 +1,22 @@
-myIntTest= 2147483648
+myIntTest = 2147483648
 
 
-
-
-
-divided=(int)(myIntTest/10)
-divided=(int)(myIntTest/100)
+divided = (int)(myIntTest/10)
+divided = (int)(myIntTest/100)
 print(divided)
+
+
+testArr = []
+
+testArr.append(1)
+testArr.append(2)
+testArr.append(2)
+testArr.append(4)
+
+testArr.append(2)
+testArr.append(4)
+
+# testArr.pop(1)
+
+
+print(testArr)
