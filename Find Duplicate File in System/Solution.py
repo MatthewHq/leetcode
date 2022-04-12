2f613d2fed11ad4ae6c63910d542c5acd7e6b6d0
MatthewHoque <hoque.matt@gmail.com>
2022-04-12 15:37:24 -0700

    additional planning done, taking break

--- Find Duplicate File in System/Solution.py
+++ Find Duplicate File in System/Solution.py
@@ -1,3 +1,18 @@
 class Solution:
-    def longestPalindrome(self, s: str) -> str:
-        
+    def findDuplicate(self, paths):
+
+        # was intially heistant to use len() but turns out its O(1)
+
+        counter = 0
+        while(counter < len(paths)):
+            charCounter = 0
+            while(charCounter < len(paths[counter])):
+                print(paths[counter][charCounter])
+                charCounter += 1
+            counter += 1
+
+
+sol = Solution()
+
+sol.findDuplicate(["root/a 1.txt(abcd) 2.txt(efgh)",
+                  "root/c 3.txt(abcd)", "root/c/d 4.txt(efgh)", "root 4.txt(efgh)"])
