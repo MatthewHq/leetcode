f975f2583044f4df0552a1c7d004470d77bdd772
MatthewHoque <hoque.matt@gmail.com>
2022-04-21 20:12:16 -0700

    Accepted, needs cleanup

--- Longest Arithmetic Subsequence/Solution.py
+++ Longest Arithmetic Subsequence/Solution.py
@@ -9,19 +9,25 @@ class Solution:
             if ssThreads.get(nums[i]) != None:
                 toPop = []
                 for storedStep in ssThreads.get(nums[i]).keys():
-                    # print("stored key", storedStep)
-                    print("data", UUIDs[ssThreads.get(nums[i]).get(storedStep)][1])
-                    UUIDs[ssThreads.get(nums[i]).get(storedStep)][0] = nums[i]
-                    UUIDs[ssThreads.get(nums[i]).get(storedStep)][1] = UUIDs[ssThreads.get(nums[i]).get(storedStep)][1]+1
-                    newExpected = storedStep+nums[i]
-                    print("HEY ", UUIDs[ssThreads.get(nums[i]).get(storedStep)][1], i, nums[i], storedStep, newExpected)
-                    if ssThreads.get(newExpected) == None:
-                        ssThreads[newExpected] = {}
-                    if ssThreads.get(newExpected).get(storedStep) == None or UUIDs[ssThreads.get(newExpected).get(storedStep)][1]<UUIDs[ssThreads.get(nums[i]).get(storedStep)][1]:
-                        ssThreads.get(newExpected)[storedStep] = ssThreads.get(nums[i]).get(storedStep)
-                    toPop.append(storedStep)
-                    if UUIDs[ssThreads.get(nums[i]).get(storedStep)][1] > subSeqMax:
-                        subSeqMax = UUIDs[ssThreads.get(nums[i]).get(storedStep)][1]
+                    if storedStep==0:
+                        UUIDs[ssThreads.get(nums[i]).get(storedStep)][1] = UUIDs[ssThreads.get(nums[i]).get(storedStep)][1]+1
+                        if UUIDs[ssThreads.get(nums[i]).get(storedStep)][1] > subSeqMax:
+                            subSeqMax = UUIDs[ssThreads.get(nums[i]).get(storedStep)][1]
+                        # print("DEFAULTING STOREDSTEP")
+                    else:
+                        # print("stored key", storedStep)
+                        # print("data", UUIDs[ssThreads.get(nums[i]).get(storedStep)][1])
+                        UUIDs[ssThreads.get(nums[i]).get(storedStep)][0] = nums[i]
+                        UUIDs[ssThreads.get(nums[i]).get(storedStep)][1] = UUIDs[ssThreads.get(nums[i]).get(storedStep)][1]+1
+                        newExpected = storedStep+nums[i]
+                        # print("HEY ", "count:",UUIDs[ssThreads.get(nums[i]).get(storedStep)][1]," i:", i, " nums[i]:",nums[i], " storedStep:",storedStep, " newExpected:",newExpected)
+                        if ssThreads.get(newExpected) == None:
+                            ssThreads[newExpected] = {}
+                        if ssThreads.get(newExpected).get(storedStep) == None or UUIDs[ssThreads.get(newExpected).get(storedStep)][1]<UUIDs[ssThreads.get(nums[i]).get(storedStep)][1]:
+                            ssThreads.get(newExpected)[storedStep] = ssThreads.get(nums[i]).get(storedStep)
+                        toPop.append(storedStep)
+                        if UUIDs[ssThreads.get(nums[i]).get(storedStep)][1] > subSeqMax:
+                            subSeqMax = UUIDs[ssThreads.get(nums[i]).get(storedStep)][1]
 
                 for pop in toPop:
                     ssThreads.get(nums[i]).pop(pop)
@@ -38,25 +44,26 @@ class Solution:
                     ssThreads.get(xpected)[step] = len(UUIDs)-1
                 
                 # print(i, j, nums[i]+nums[i]-nums[j])
-                print("=======", " i:",i, " nums[i]:",nums[i]," step:", step," xpected:",xpected)
-                print(ssThreads)
-                print(UUIDs)
-                print("max ", subSeqMax+1)
+                # print("=======", " i:",i, " nums[i]:",nums[i]," step:", step," xpected:",xpected)
+                # print(ssThreads)
+                # print(UUIDs)
+        # print("max ", subSeqMax+1)
         return subSeqMax+1
 
 
 sol = Solution()
-# sol.longestArithSeqLength([83, 20, 17, 43, 52, 78, 68, 45])
-# sol.longestArithSeqLength([3, 6, 9, 12])
-# sol.longestArithSeqLength([10, 15, 20, 25, 30, 35, 40, 50, 60])
-# # sol.longestArithSeqLength([24, 13, 1, 100, 0,0,0])
-# sol.longestArithSeqLength([24, 13, 1, 100, 0, 94, 3, 0, 3])
-# sol.longestArithSeqLength([22, 8, 57, 41, 36, 46, 42, 28, 42, 14, 9, 43, 27, 51, 0, 0, 38, 50, 31, 60, 29, 31, 20, 23, 37, 53, 27, 1, 47, 42, 28, 31,
-#                           10, 35, 39, 12, 15, 6, 35, 31, 45, 21, 30, 19, 5, 5, 4, 18, 38, 51, 10, 7, 20, 38, 28, 53, 15, 55, 60, 56, 43, 48, 34, 53, 54, 55, 14, 9, 56, 52])
+sol.longestArithSeqLength([83, 20, 17, 43, 52, 78, 68, 45]) #2
+sol.longestArithSeqLength([3, 6, 9, 12])#4
+sol.longestArithSeqLength([10, 15, 20, 25, 30, 35, 40, 50, 60])#7
+# sol.longestArithSeqLength([24, 13, 1, 100, 0,0,0])
+sol.longestArithSeqLength([24, 13, 1, 100, 0, 94, 3, 0, 3])#2
+sol.longestArithSeqLength([22, 8, 57, 41, 36, 46, 42, 28, 42, 14, 9, 43, 27, 51, 0, 0, 38, 50, 31, 60, 29, 31, 20, 23, 37, 53, 27, 1, 47, 42, 28, 31,
+                          10, 35, 39, 12, 15, 6, 35, 31, 45, 21, 30, 19, 5, 5, 4, 18, 38, 51, 10, 7, 20, 38, 28, 53, 15, 55, 60, 56, 43, 48, 34, 53, 54, 55, 14, 9, 56, 52])#6
 
-sol.longestArithSeqLength([1,1,0,0,1,0,0,1,0])
-# sol.longestArithSeqLength([1,1,0,0,1,0,0,1,0,1,0,1,1,1,0,1,1,0,1,1,0,0,1,1,0,1,0,1,1,0,1,1,1,1,1,0,1,0,0,0,0,0,0,0,1,0,0,1,1,1,0,0,0,0,0,1,1,1,0,0,1,1,1,1,1,0,0,0,1,1,1,1,1,1,0,1,1,0,0,0,1,1,1,1,0,0,1,1,1,0,0,0,1,0,1,0,1,1,1,0,1,1,0,1,1,1,0,1,1,1,0,1,0,1,0,0,0,1,1,0,0,0,0,1,1,0,0,1,0,0,1,0,1,0,0,0,0,0,1,1,1,1,1,0,1,1,0,0,0,0,0,0,1,1,1,0,1,1,0,1,1,1,0,1,0,1,0,0,1,0,0,1,0,0,1,1,0,1,0,0,1,1,0,1,0,0,0,1,0,1,1,1,1,1,0,0,0,1,0,0,1,0,1,1,1,1,0,1,1,0,0,0,0,1,0,1,0,1,1,1,1,1,0,0,0,0,0,1,0,0,0,1,0,1,0,0,0,0,0,1,1,1,0,0,1,1,1,0,1,1,0,1,1,1,0,1,0,0,1,0,1,0,0,1,0,1,1,1,1,1,1,1,1,0,1,0,1,1,1,0,1,1,0,1,0,0,0,1,0,1,0,0,0,0,1,0,0,0,1,0,1,1,1,0,1,0,0,1,0,1,0,0,0,1,0,1,0,1,1,0,0,1,1,0,0,1,0,1,1,1,1,0,1,1,1,0,0,0,1,0,0,1,1,0,0,1,0,0,0,0,0,0,1,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,0,1,1,1,1,0,0,0,1,0,0,0,1,1,0,1,0,1,0,0,0,0,1,1,0,0,1,1,0,0,1,1,1,0,0,0,0,1,0,1,1,1,1,1,1,1,1,1,1,1,0,1,0,1,1,0,0,1,0,0,0,1,0,0,0,0,1,1,1,0,1,1,0,0,1,0,0,1,0,0,1,0,0,1,1,0,0,1,1,0,0,0,1,1,0,1,0,0,0,0,0,1,0,0,0,1,0,0,0,1,1,1,0,1,1,0,1,1,0,0,0,1,0,0,0,0,1,0,0,0,1,0,0,0,0,0,1,0,1,1,0,1,0,1,0,1,0,0,0,0,1,1,0,0,0,1,1,0,1,1,0,1,1,0,0,0,0,1,1,0,0,1,1,0,0,0,0,0,0,0,1,0,1,1,1,0,0,0,1,1,0,1,0,0,1,0,0,0,1,1,0,0,0,0,1,1,0,0,1,1,0,1,0,0,0,1,1,0,1,0,0,0,1,1,1,1,1])
+sol.longestArithSeqLength([1,1,0,0,1,0,0,1,0,1,0,1,1,1])#8
 
+sol.longestArithSeqLength([1,1,0,0,1,0,0,1,0,1,0,1,1,1,0,1,1,0,1,1,0,0,1,1,0,1,0,1,1,0,1,1,1,1,1,0,1,0,0,0,0,0,0,0,1,0,0,1,1,1,0,0,0,0,0,1,1,1,0,0,1,1,1,1,1,0,0,0,1,1,1,1,1,1,0,1,1,0,0,0,1,1,1,1,0,0,1,1,1,0,0,0,1,0,1,0,1,1,1,0,1,1,0,1,1,1,0,1,1,1,0,1,0,1,0,0,0,1,1,0,0,0,0,1,1,0,0,1,0,0,1,0,1,0,0,0,0,0,1,1,1,1,1,0,1,1,0,0,0,0,0,0,1,1,1,0,1,1,0,1,1,1,0,1,0,1,0,0,1,0,0,1,0,0,1,1,0,1,0,0,1,1,0,1,0,0,0,1,0,1,1,1,1,1,0,0,0,1,0,0,1,0,1,1,1,1,0,1,1,0,0,0,0,1,0,1,0,1,1,1,1,1,0,0,0,0,0,1,0,0,0,1,0,1,0,0,0,0,0,1,1,1,0,0,1,1,1,0,1,1,0,1,1,1,0,1,0,0,1,0,1,0,0,1,0,1,1,1,1,1,1,1,1,0,1,0,1,1,1,0,1,1,0,1,0,0,0,1,0,1,0,0,0,0,1,0,0,0,1,0,1,1,1,0,1,0,0,1,0,1,0,0,0,1,0,1,0,1,1,0,0,1,1,0,0,1,0,1,1,1,1,0,1,1,1,0,0,0,1,0,0,1,1,0,0,1,0,0,0,0,0,0,1,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,0,1,1,1,1,0,0,0,1,0,0,0,1,1,0,1,0,1,0,0,0,0,1,1,0,0,1,1,0,0,1,1,1,0,0,0,0,1,0,1,1,1,1,1,1,1,1,1,1,1,0,1,0,1,1,0,0,1,0,0,0,1,0,0,0,0,1,1,1,0,1,1,0,0,1,0,0,1,0,0,1,0,0,1,1,0,0,1,1,0,0,0,1,1,0,1,0,0,0,0,0,1,0,0,0,1,0,0,0,1,1,1,0,1,1,0,1,1,0,0,0,1,0,0,0,0,1,0,0,0,1,0,0,0,0,0,1,0,1,1,0,1,0,1,0,1,0,0,0,0,1,1,0,0,0,1,1,0,1,1,0,1,1,0,0,0,0,1,1,0,0,1,1,0,0,0,0,0,0,0,1,0,1,1,1,0,0,0,1,1,0,1,0,0,1,0,0,0,1,1,0,0,0,0,1,1,0,0,1,1,0,1,0,0,0,1,1,0,1,0,0,0,1,1,1,1,1])
+#306
 
 
 
