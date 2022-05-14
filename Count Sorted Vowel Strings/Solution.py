a9d0ca3c2410b251c46d47edb7a57a7990c69d8c
MatthewHoque <hoque.matt@gmail.com>
2022-05-13 17:53:44 -0700

    more dp pracice, medium

---
+++ Count Sorted Vowel Strings/Solution.py
@@ -0,0 +1,22 @@
+class Solution:
+    def countVowelStrings(self, n: int) -> int:
+        vowelChart=[[1,1,1,1,1]]
+        
+
+        
+        for i in range(1,n):
+            newRow=[]
+            newRow.append(1)
+            rollingSum=1
+            for j in range(1,5):
+                rollingSum+=vowelChart[i-1][j]
+                newRow.append(rollingSum)
+            vowelChart.append(newRow)
+        # print(vowelChart)
+
+        return sum(vowelChart[n-1])
+
+
+
+sol =Solution()
+print(sol.countVowelStrings(33))
