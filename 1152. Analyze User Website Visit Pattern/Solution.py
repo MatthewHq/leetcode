115ba55c622e0294dd5b6ec6b09998b1b290bf22
MatthewHoque <hoque.matt@gmail.com>
2022-08-24 21:04:38 -0700

    setting up #1152

---
+++ 1152. Analyze User Website Visit Pattern/Solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def mostVisitedPattern(self, username, timestamp, website):
+        pass
+
+
+username = ["joe", "joe", "joe", "james", "james", "james", "james", "mary", "mary", "mary"]
+timestamp = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+website = ["home", "about", "career", "home", "cart", "maps", "home", "home", "about", "career"]
