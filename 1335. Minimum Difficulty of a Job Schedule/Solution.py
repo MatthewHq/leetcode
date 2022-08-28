7160537de60c4bbcb123e517c1428fc20713794a
MatthewHoque <hoque.matt@gmail.com>
2022-08-28 06:29:06 -0700

    #1335 working without knowing edge cases / submitting

--- 1335. Minimum Difficulty of a Job Schedule/Solution.py
+++ 1335. Minimum Difficulty of a Job Schedule/Solution.py
@@ -1,27 +1,104 @@
 class Solution:
     def minDifficulty(self, jobDifficulty, d):
-        srResults=self.splitRange(0, len(jobDifficulty), jobDifficulty)
-        print(srResults)
-        firstMin=self.getFirstMin(srResults)
-        print(firstMin)
-    def splitRange(self, start, end, array):
+        if d > len(jobDifficulty):
+            return -1
+        print(jobDifficulty)
+        cuts=[]
+        nextRange=[]
+        for i in range(d-1):
+            nextRange=self.makeRanges(cuts,jobDifficulty)
+            print("nextRange",nextRange)
+            srResults=self.splitRange(nextRange, jobDifficulty)
+            print("srResults",srResults)
+            firstMin=self.getFirstMin(srResults)
+            print("firstMin",firstMin)
+            cuts.append(firstMin)
+        # firstRange=[[0, len(jobDifficulty)]]
+        # srResults=self.splitRange(firstRange, jobDifficulty)
+        # firstMin=self.getFirstMin(srResults)
+        
+
+        
+        # print(srResults)
+        
+        # print(firstMin)
+        # cuts.append(2)
+        # cuts.append(firstMin)
+        # cuts.append(5)
+        
+        # print(self.makeRanges(cuts,jobDifficulty))
+        print(cuts)
+        return self.getRangeSums(cuts,jobDifficulty)
+
+
+        
+
+    def splitRange(self,rangeList, array):
+        print("====SPLITTING RANGES====",rangeList)
         splitVals = []
-        for i in range(end-start-1):
-            x = i+start+1
-            #note instead of max() there can be a more memory intensive way to save on processing to achieve slightly better results, though it would be more complicated
-            splitVals.append([max(array[start:x]), max(array[x:end])])
-            print(array[start:x], array[x:end])
+        for rangePair in rangeList:
+            print("==spliting==",rangePair)
+            start=rangePair[0]
+            end=rangePair[1]+1
+            for i in range(end-start-1):
+                x = i+start+1
+                #note instead of max() there can be a more memory intensive way to save on processing to achieve slightly better results, though it would be more complicated
+                # print(array[start:x],array[x:end],start,end,x)
+                splitVals.append([max(array[start:x]), max(array[x:end]),x-1])
+                print("range Made",array[start:x], array[x:end])
         return splitVals
 
     def getFirstMin(self,srResults):
         minIndex=0
         minVal=None
         for i in range(len(srResults)):
-            added=sum(srResults[i])
+            added=srResults[i][0]+srResults[i][1]
             if minVal==None or added<minVal:
                 minVal=added
                 minIndex=i
-        return minIndex
+        return srResults[minIndex][2]
+
+    def getRangeSums(self,cuts,arr):
+        cuts.sort()
+        result=0
+        start=0
+        for cut in cuts:
+            rg=arr[start:cut+1]
+            print("len",len(rg),rg)
+            if len(rg)>1:
+                print("adding",max(rg))
+                result+=max(rg)
+            else:
+                print("adding",arr[cut])
+                result+=arr[cut]
+            start=cut+1
+        lastRg=arr[start:len(arr)]
+        #print("len",len(rg),rg)
+        if len(lastRg)>1:
+                print("adding",max(lastRg))
+                result+=max(lastRg)
+        else:
+                print("adding",arr[len(arr)-1])
+                result+=arr[len(arr)-1]
+        return result
+
+
+        
+    
+    def makeRanges(self,cuts,arr):
+        cuts.sort()
+        start=0
+        rangeList=[]
+        for i in cuts:
+            end=i
+            rangeList.append([start,end])
+            start=end+1
+        rangeList.append([start,len(arr)-1])
+        return rangeList
+            
+
+
+        
 
 
 
@@ -29,7 +106,9 @@ class Solution:
 sol = Solution()
 
 
-diffs = [1, 2, 3, 4, 5, 6]
-diffs = [1,5,2,4,8,1,7]
-diffs = [10,5,2,4,8,1,7]
-sol.minDifficulty(diffs, 2)
+jobDifficulty = [1, 2, 3, 4, 5, 6]
+jobDifficulty = [1,5,2,4,8,1,7]
+jobDifficulty = [10,5,2,4,8,1,7]
+jobDifficulty = [6,5,4,3,2,1]
+jobDifficulty = [1,1]
+print(sol.minDifficulty(jobDifficulty, 1))
---
+++ 1335. Minimum Difficulty of a Job Schedule/testing.py
@@ -0,0 +1,5 @@
+test=[0,1,2,3,4,5]
+
+
+print(test[0:2])
+print(test[2:4])
