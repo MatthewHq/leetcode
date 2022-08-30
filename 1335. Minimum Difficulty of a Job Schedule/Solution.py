182455d4696ba3253a541c75c236d5ce79116f0d
MatthewHoque <hoque.matt@gmail.com>
2022-08-30 06:22:40 -0700

    #1335 Attempt Failed, pausing here

--- 1335. Minimum Difficulty of a Job Schedule/Solution.py
+++ 1335. Minimum Difficulty of a Job Schedule/Solution.py
@@ -26,7 +26,8 @@ class Solution:
         # cuts.append(firstMin)
         # cuts.append(5)
         
-        # print(self.makeRanges(cuts,jobDifficulty))
+        # print(self.makeRanges(cuts,jobDifficulty))\
+        cuts.sort()
         print(cuts)
         return self.getRangeSums(cuts,jobDifficulty)
 
@@ -34,7 +35,7 @@ class Solution:
         
 
     def splitRange(self,rangeList, array):
-        print("====SPLITTING RANGES====",rangeList)
+        #print("====SPLITTING RANGES====",rangeList)
         splitVals = []
         for rangePair in rangeList:
             print("==spliting==",rangePair)
@@ -45,17 +46,20 @@ class Solution:
                 #note instead of max() there can be a more memory intensive way to save on processing to achieve slightly better results, though it would be more complicated
                 # print(array[start:x],array[x:end],start,end,x)
                 splitVals.append([max(array[start:x]), max(array[x:end]),x-1])
-                print("range Made",array[start:x], array[x:end])
+                #print("range Made",array[start:x], array[x:end])
         return splitVals
 
     def getFirstMin(self,srResults):
         minIndex=0
+        addeds=[] #DEBUG
         minVal=None
         for i in range(len(srResults)):
             added=srResults[i][0]+srResults[i][1]
+            addeds.append(added) #DEBUG
             if minVal==None or added<minVal:
                 minVal=added
                 minIndex=i
+        print("ALL ADDEEDSSS",addeds)#DEBUG
         return srResults[minIndex][2]
 
     def getRangeSums(self,cuts,arr):
@@ -64,21 +68,21 @@ class Solution:
         start=0
         for cut in cuts:
             rg=arr[start:cut+1]
-            print("len",len(rg),rg)
+            #print("len",len(rg),rg)
             if len(rg)>1:
-                print("adding",max(rg))
+                #print("adding",max(rg))
                 result+=max(rg)
             else:
-                print("adding",arr[cut])
+                #print("adding",arr[cut])
                 result+=arr[cut]
             start=cut+1
         lastRg=arr[start:len(arr)]
         #print("len",len(rg),rg)
         if len(lastRg)>1:
-                print("adding",max(lastRg))
+                #print("adding",max(lastRg))
                 result+=max(lastRg)
         else:
-                print("adding",arr[len(arr)-1])
+                #print("adding",arr[len(arr)-1])
                 result+=arr[len(arr)-1]
         return result
 
@@ -108,7 +112,8 @@ sol = Solution()
 
 jobDifficulty = [1, 2, 3, 4, 5, 6]
 jobDifficulty = [1,5,2,4,8,1,7]
-jobDifficulty = [10,5,2,4,8,1,7]
-jobDifficulty = [6,5,4,3,2,1]
-jobDifficulty = [1,1]
-print(sol.minDifficulty(jobDifficulty, 1))
+# jobDifficulty = [10,5,2,4,8,1,7]
+# jobDifficulty = [6,5,4,3,2,1]
+# jobDifficulty = [1,1]
+jobDifficulty=[186,398,479,206,885,423,805,112,925,656,16,932,740,292,671,360]
+print(sol.minDifficulty(jobDifficulty, 4))
