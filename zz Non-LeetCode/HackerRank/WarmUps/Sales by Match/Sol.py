686ea8aead1075ac5e691c47842124ccecda1e08
MatthewHoque <hoque.matt@gmail.com>
2022-08-30 11:37:21 -0700

    hackerrank sales by match warmup complete saved file

--- zz Non-LeetCode/HackerRank/WarmUps/Sales by Match/Sol.py
+++ zz Non-LeetCode/HackerRank/WarmUps/Sales by Match/Sol.py
@@ -2,11 +2,11 @@ def sockMerchant(n, ar):
     # Write your code here
     pairCount=0
     sockpairs={}
-    for i in range(len(arr)):
-        if sockpairs.get(arr[i]):
-            sockpairs[arr[i]]+=1
+    for i in range(len(ar)):
+        if sockpairs.get(ar[i]):
+            sockpairs[ar[i]]+=1
         else:
-            sockpairs[arr[i]]=1
+            sockpairs[ar[i]]=1
     for val in sockpairs.values():
         pairCount+=(int)(val/2)
     return pairCount
