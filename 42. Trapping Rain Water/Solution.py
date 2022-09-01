6a1f78589efec685ad77ea9754fe9560e8ea0c44
MatthewHoque <hoque.matt@gmail.com>
2022-09-01 15:20:59 -0700

    Looked at answer to save time, will have to come back later after forgot, initial approach was a combination of two solutions and seemed to be right track

--- 42. Trapping Rain Water/Solution.py
+++ 42. Trapping Rain Water/Solution.py
@@ -1,3 +1,30 @@
 class Solution:
-    def longestPalindrome(self, s: str) -> str:
+    def trap(self, height):
+        # double array of [[start,end]]
+        cutsQ = [[0, len(height)-1]]
+        wholeSet=cutsQ.pop()
+        largeSet=self.get2Largest(wholeSet, height)
         
+        print(largeSet)
+        self.produceCuts(largeSet,wholeSet,height,cutsQ)
+
+    def produceCuts(self, largeSet, wholeSet, arr, cutsQueue):
+        pass
+
+    # return 2 largest indices in one double array
+    def get2Largest(self, wholeSet, arr):
+        large = [[0, None], [0, None]]
+        for i in range(wholeSet[1]-wholeSet[0]):
+            if arr[i] >= large[0][0]:
+                large[1] = large[0]
+                large[0] = [arr[i], i]
+            elif arr[i] >= large[1][0]:
+                large[1] = [arr[i], i]
+        return large
+
+
+sol = Solution()
+
+
+height = [1, 0, 6, 0, 1, 0, 5, 0, 1, 1, 2]
+print(sol.trap(height))
