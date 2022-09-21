0d6d1b07ca3ca2b2a6f1b3cf37f037071f3beb0c
MatthewHoque <hoque.matt@gmail.com>
2022-09-21 14:27:00 -0700

    stack approach for #20

--- 20. Valid Parentheses/Solution.py
+++ 20. Valid Parentheses/Solution.py
@@ -1,51 +1,33 @@
 class Solution:
     def isValid(self, s):
-        parenthesi = 0
-        brace = 0
-        bracket = 0
-        output = 'None'
-
-        #make expected a STACK
-
+        pairs = {
+            '(': ')',
+            '[': ']',
+            '{': '}',
+            ')': '1',
+            ']': '1',
+            '}': '1'
+        }
+        expectedStack = []
         for c in s:
-            # PARENTHESI
-            if c == '(':  # open
-                parenthesi += 1
-                expected = ')'
-            elif c == ')':  # --close
-                if parenthesi == 0 or expected != ')':
-                    print("A")
-                    return False
-                parenthesi -= 1
 
-            # BRACE
-            elif c == '{':  # open
-                brace += 1
-                expected = '}'
-            elif c == '}':  # --close
-                if brace == 0 or expected != '}':
+            if pairs[c] == '1':
+                if len(expectedStack) != 0 and expectedStack[len(expectedStack)-1] == c:
+                    expectedStack.pop()
+                else:
                     return False
-                brace -= 1
-
-            # BRACKET
-            elif c == '[':  # open
-                bracket += 1
-                expected = ']'
-            elif c == ']':  # --close
-                if bracket == 0 or expected != ']':
-                    return False
-                bracket -= 1
-
-        # print(parenthesi,brace,bracket)
-        if parenthesi != 0 or brace != 0 or bracket != 0:
+            else:
+                expectedStack.append(pairs[c])
+        if len(expectedStack) != 0:
             return False
         return True
 
 
-s = "((((()()))))"
-s = "([{}])"
-s = "([)]"
-s = "()[]{}"
-s="{[]}"
+# s = "((((()()))))"
+# s = "([{}])"
+# s = "([)]"
+# s = "()[]{}"
+# s = "{[]}"
+s = "]"
 sol = Solution()
 print(sol.isValid(s))
