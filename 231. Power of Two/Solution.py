ada55705498d1ddbeb1cdf37967aebea2266c0b7
MatthewHoque <hoque.matt@gmail.com>
2022-09-22 12:36:49 -0700

    #231 solved not optimal solution though

--- 231. Power of Two/Solution.py
+++ 231. Power of Two/Solution.py
@@ -1,6 +1,20 @@
 class Solution:
-    def longestPalindrome(self, s: str) -> str:
-        pass
+    def isPowerOfTwo(self, n: int) -> bool:
+        if n==0:
+            return False
+        flag=False
+        i=0
+        while (n>>i)!=0:
+            if 1 & (n>>i) ==1:
+                if not flag:
+                    flag = True
+                else:
+                    return False
+            i+=1
+        return True
 
 sol = Solution()
 
+# print(sol.isPowerOfTwo(17))
+# print("---")
+print(sol.isPowerOfTwo(255))
