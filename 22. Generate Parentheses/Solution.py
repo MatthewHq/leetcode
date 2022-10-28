f86e508461f6a5e874daa4f56061080f8c1845d0
MatthewHoque <hoque.matt@gmail.com>
2022-10-27 21:51:03 -0700

    another casual day, progress on #22

--- 22. Generate Parentheses/Solution.py
+++ 22. Generate Parentheses/Solution.py
@@ -1,6 +1,24 @@
 class Solution:
     def generateParenthesis(self, n):
-        pass
+        #small note, there is a way to make this with dynamic programming for even faster performance
+        trackerLimits=[]
+        trackers=[]
+        for i in range(n):
+            trackerLimits.append(i*2)
+            trackers.append(i)
+
+        print(trackerLimits)
+        print(trackers)
+
+        #curr will target a specific slot in the trackers array and max it out, starting from the far end
+        curr=n-1
+        while trackerLimits!=trackers:
+            if trackers[curr]!=trackerLimits[curr]:
+                
+
 
 sol = Solution()
+sol.generateParenthesis(4)
+
+
 
