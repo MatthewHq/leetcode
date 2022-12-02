cd7f0177d2cc852cac79fd581b5021babe5f29f1
MatthewHoque <hoque.matt@gmail.com>
2022-12-02 14:26:22 -0800

    completed 26

--- 26. Remove Duplicates from Sorted Array/Solution.py
+++ 26. Remove Duplicates from Sorted Array/Solution.py
@@ -1,6 +1,23 @@
 class Solution:
-    def longestPalindrome(self, s: str) -> str:
-        pass
+    def removeDuplicates(self, nums) -> int:
+        if len(nums)==1:
+            return 1
+
+        current=nums[0]
+        nextIndex=1
+
+        for i in range(len(nums)):
+            if nums[i]!=current:
+                nums[nextIndex]=nums[i]
+                nextIndex+=1
+                current=nums[i]
+        return nextIndex
+
+
+
 
 sol = Solution()
+nums = [0,0,1,1,1,2,2,3,3,4]
+nums = [1,1,2]
 
+print(sol.removeDuplicates(nums))
