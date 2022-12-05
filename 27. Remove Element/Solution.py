ee8f5c6501a7e73de153c76288997b51f11dcabf
MatthewHoque <hoque.matt@gmail.com>
2022-12-04 19:09:59 -0800

    finished 27 a while ago

--- 27. Remove Element/Solution.py
+++ 27. Remove Element/Solution.py
@@ -1,6 +1,28 @@
 class Solution:
-    def longestPalindrome(self, s: str) -> str:
-        pass
+    def removeElement(self, nums, val: int) -> int:
+        if len(nums)==1:
+            if nums[0]==val:
+                return 0  
+            else: #for readability lol
+                return 1
+        currentIndex=None
+
+        for i in range(len(nums)):
+            if nums[i]!=val:
+                if currentIndex!=None:
+                    nums[currentIndex]=nums[i]
+                    nums[i]=val
+                    currentIndex+=1
+            elif currentIndex==None:
+                currentIndex=i
+
+        print(nums)
+        return currentIndex
 
 sol = Solution()
+nums = [3,2,2,3]
+nums = [3,3,3,2,1,3,2,3,4]
+
+print(sol.removeElement(nums,3))
+
 
