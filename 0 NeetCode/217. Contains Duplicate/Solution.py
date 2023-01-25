148a2b9f865156201baeacd78a199f45770e5f9d
MatthewHoque <hoque.matt@gmail.com>
2023-01-24 16:24:41 -0800

    another solution, less cheesy

--- 0 NeetCode/217. Contains Duplicate/Solution.py
+++ 0 NeetCode/217. Contains Duplicate/Solution.py
@@ -1,17 +1,17 @@
 class Solution:
     def containsDuplicate(self, nums) -> bool:
-        dupeHash={}
-        containsDupe=False
-        for num in nums:
-            if dupeHash.get(num) == None:
-                dupeHash[num]=True
-            else:
-                containsDupe=True
-        return containsDupe
+        dupeSet=set()
+        for i in nums:
+            if i in dupeSet:
+                return False
+            dupeSet.add(i)
+        return True
+            
 
 sol = Solution()
 
 arr=[1,2,3,4,4]
 arr2=[1,2,3,4,5]
+print(sol.containsDuplicate(arr))
 print(sol.containsDuplicate(arr2))
         
