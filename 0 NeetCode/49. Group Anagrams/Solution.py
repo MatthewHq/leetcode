f455354eff9cab4c157316b67eb9119669da92d3
MatthewHoque <hoque.matt@gmail.com>
2023-01-31 16:01:47 -0800

    small improvement after looking at solution which was pleasantly my approach

--- 0 NeetCode/49. Group Anagrams/Solution.py
+++ 0 NeetCode/49. Group Anagrams/Solution.py
@@ -1,37 +1,35 @@
 class Solution:
     def groupAnagrams(self, strs):
-        groups={}
+        groups = {}
         for anaStr in strs:
-            asKey=self.strToKey(anaStr)
-            if groups.get(asKey)==None:
-                groups[asKey]=[anaStr]
+            asKey = self.strToKey(anaStr)
+            if groups.get(asKey) == None:
+                groups[asKey] = [anaStr]
             else:
                 groups[asKey].append(anaStr)
-        groupedAnas=[]
+        groupedAnas = []
         for group in groups.values():
             groupedAnas.append(group)
         return groupedAnas
 
-    def strToKey(self,cStr):
-        arr=[0 for x in range(26)]
+    def strToKey(self, cStr):
+        arr = [0]*26
         for c in cStr:
-            arr[ord(c)-97]+=1
-        keyStr=""
+            arr[ord(c)-97] += 1
+        keyStr = ""
         for i in arr:
-            keyStr+=str(i)
-            keyStr+='|'
+            keyStr += str(i)
+            keyStr += '|'
         return keyStr
-        
 
-strs = ["bdddddddddd","bbbbbbbbbbc"]
+
+strs = ["bdddddddddd", "bbbbbbbbbbc"]
 
 sol = Solution()
 print(sol.groupAnagrams(strs))
 
 
-
-
-#eat
+# eat
 #
 #  {1,2,3,4,5,6.....K}
 #  [a,b,c,d,e]
