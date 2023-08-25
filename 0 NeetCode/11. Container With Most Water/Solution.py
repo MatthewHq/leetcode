866d635d215c714ba9a3535af614469218d608fc
MatthewHoque <hoque.matt@gmail.com>
2023-08-25 11:13:10 -0700

    11.Container With Most Water started and completed

---
+++ 0 NeetCode/11. Container With Most Water/Solution.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        l, r = 0, (len(height)-1)
+        print(l, r)
+        waterMax = 0
+        while l < r:
+            currentWater = (r-l)*min(height[l], height[r])
+            if currentWater > waterMax:
+                waterMax = currentWater
+            if height[l] < height[r]:
+                l += 1
+            else:
+                r -= 1
+        return waterMax
+
+
+sol = Solution()
+height = [1, 8, 6, 2, 5, 4, 8, 3, 7]
+print(sol.maxArea(height))
