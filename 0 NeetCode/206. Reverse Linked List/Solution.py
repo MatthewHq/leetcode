1b37ac2014c9ff6c2701995e0f8f1421b26e7f11
MatthewHoque <hoque.matt@gmail.com>
2023-08-30 09:03:10 -0700

    completed 206. Reversed Linked List

---
+++ 0 NeetCode/206. Reverse Linked List/Solution.py
@@ -0,0 +1,61 @@
+from typing import List,Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head==None:
+            return None
+        if head.next==None:
+            return head
+        prior=ListNode(head.val,None)
+        while head.next!=None:
+            head=head.next
+            prior=ListNode(head.val,prior)
+        return prior
+    # 1 2 3 4 
+    # *1(2) <- _(1,)
+    # 1 *2(3) <- _
+
+    #4 3 2 1
+
+    def printLinkedList(self,l):
+        print("=====")
+        while l !=None:
+            print(l.val)
+            l=l.next
+        print("=====")
+
+    def createLinkedList(self,arr):
+        if len(arr)==0:
+            return None
+        head=ListNode(arr[0],None)
+        builder=head
+        for i in range(1,len(arr)):
+            temp=ListNode(arr[i],None)
+            builder.next=temp
+            builder=builder.next
+        
+        # print(head)
+        return head
+            
+            
+
+
+sol = Solution()
+
+arr=[]
+l=sol.createLinkedList(arr)
+sol.printLinkedList(l)
+
+rev=sol.reverseList(l)
+sol.printLinkedList(rev)
+
+
+
+# 1 2 3 4 5 6
+
+
