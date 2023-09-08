eb1279d89dd3839a6ae1dad8672bc55f57b9c050
MatthewHoque <hoque.matt@gmail.com>
2023-09-08 14:49:15 -0700

    saving completed 19 before slight optimization test

--- 0 NeetCode/19. Remove Nth Node From End of List/Solution.py
+++ 0 NeetCode/19. Remove Nth Node From End of List/Solution.py
@@ -5,11 +5,37 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-
+    
 class Solution:
-    def longestPalindrome(self, s: str) -> str:
-        pass
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        # keep=head
+        prev=None
+        forward=head
+        for i in range(n-1):
+            forward=forward.next
 
+        while forward.next:
+            forward=forward.next
+            if not prev:
+                prev=head
+            # keep=keep.next
+            
+        
+
+        
+        # self.printLinkedList(prev)
+        # self.printLinkedList(keep)
+        # self.printLinkedList(forward)
+        
+        if prev==None:
+            return head.next
+        else:    
+            prev.next=prev.next.next
+
+        # print("---")
+        return head
+
+    ##########################
     def printLinkedList(self,l):
         listed=[]
         while l !=None:
@@ -27,6 +53,16 @@ class Solution:
             builder.next=temp
             builder=builder.next
         return head
+    #############################
 
 sol = Solution()
+arr=[1,2]
+n=1
 
+# arr=[1,2]
+# n=1
+
+l1=sol.createLinkedList(arr)
+# sol.printLinkedList(l1)
+removed=sol.removeNthFromEnd(l1,n)
+sol.printLinkedList(removed)
