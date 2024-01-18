c3552a9c679ac78b6799806266ff4b4bccb5586d
MatthewHoque <hoque.matt@gmail.com>
2024-01-18 13:33:01 -0800

    different solution but misunderstood problem both past times

--- 0 NeetCode/287. Find the Duplicate Number/Solution.py
+++ 0 NeetCode/287. Find the Duplicate Number/Solution.py
@@ -1,26 +1,17 @@
 from typing import List,Optional
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        i=0
-        while i<len(nums): # < what
-            if nums[i]-1!=i:
-                print(nums)
-                holder=nums[i]
-                otherIndex=nums[i]-1
-
-                #current target set to other
-                nums[i]=nums[otherIndex]
-
-                #are they the same? found culprit
-                if nums[i]==holder:
-                    return holder
-                
-                #set other to what we were holding
-                nums[otherIndex]=holder
-                if nums[i]-1==i:
-                    i+=1
-            else:
-                i+=1
+       l=len(nums)
+    #    print(l)
+       mx=(l*(l+1))/2-1
+    #    print(mx)
+       realSum=sum(nums)
+    #    print(realSum)
+       dif=mx-realSum
+    #    print(dif)
+       dupe=l-1-dif
+    #    print(dupe)
+       return int(dupe)
 sol = Solution()
 
 
---
+++ 0 NeetCode/287. Find the Duplicate Number/thinking.py
@@ -0,0 +1,13 @@
+
+
+h=1
+for i in range(2,21):
+    arr=[]
+    for j in range(i):
+        arr.append(h)
+    
+    for k in range(i,20):
+        arr.append(k)
+    print(len(arr),arr)
+
+
